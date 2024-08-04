785d7b5fa31df8fb9dcda0dbc3c11c811432fc92
JeongWoo <46392209+jjeong723@users.noreply.github.com>
2024-08-04 21:50:30 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
+++ C++/프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -4,39 +4,14 @@
 using namespace std;
 
 int solution(int a, int b) {
-    int answer = 1;
-    
-    // 기약분수 만들기
-    int i = 1;
-    while (1)
+    int answer = 0;
+    while(b % 2 == 0)
     {
-        if (i == a + 1) break;
-        
-        if( a % i == 0 && b % i == 0)
-        {
-            a /= i;
-            b /= i;
-            i = 1;
-        }
-        i++;
+        b /= 2;
     }
-    
-    // 소인수 분해하기
-    int divider = 2;
-    while (b > 1)
+    while(b % 5 == 0)
     {
-        while (b % divider == 0)
-        {
-            // 만약 2, 5가 아닌 다른 것으로 나누어진다면 
-            // 무한소수이므로 2를 return
-            if (divider != 2 && divider != 5)
-            {
-                return 2;
-            }
-            b /= divider;
-        }
-        divider++;
+        b /= 5;
     }
-    
-    return answer;
+    return (a % b == 0)?1:2;
 }
