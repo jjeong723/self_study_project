c384bfc7a14de0384f6604cad0e475036b2adb83
JeongWoo <46392209+jjeong723@users.noreply.github.com>
2024-08-07 22:35:18 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> score) {
+    vector<int> answer;
+    map<int, int> mp;
+    
+    vector<int> v, cpy;
+    int cnt=1;
+    
+    for(int i=0; i<score.size(); i++)
+    {
+        v.push_back(score[i][0]+score[i][1]);
+        cpy.push_back(score[i][0]+score[i][1]);
+    }
+    
+    sort(cpy.begin(), cpy.end(), greater<>());
+    
+    for(int i=0; i<cpy.size(); i++)
+    {
+        if(mp.find(cpy[i])==mp.end()) mp[cpy[i]]=cnt;        
+        cnt++;
+    }
+    
+    for(int i=0; i<v.size(); i++)
+        answer.push_back(mp[v[i]]);    
+    return answer;
+}
